bf64fee558a786bd578f1edb54e5bf5309462c48
Michealbori <moluwabori448@gmail.com>
2024-04-10 10:03:44 +0100

    SPLASH WEBSITE

---
+++ app.js
@@ -0,0 +1,20 @@
+function linkedin() {
+    window.location = "https://www.linkedin.com/in/michael-o-655b88233/";
+     }
+function twitter() {
+    window.location = "https://twitter.com/M11Sikat";
+     }
+function instgram() {
+    window.location = "https://www.instagram.com/micheal_o_sikat?igsh=YzAwZjE1ZTI0Zg%3D%3D&utm_source=qr";
+     }
+
+
+// onload section!
+
+const loading = document.getElementById('custom-loader');
+
+window.addEventListener('load', () => {
+    loading.style.display = 'none';
+});
+
+loading.delay(1000);
